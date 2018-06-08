8369542f15a0be4b2889530d694c142ab6679ecc
MasterUNG <phrombutr@gmail.com>
2018-06-08 13:42:20 +0700

    Increase Number Parrent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {ChildcomponentComponent} from './components/childcomponent/childcomponent.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'], entryComponents: [ChildcomponentComponent]
 })
 export class AppComponent {
-  title = 'app';
-}
+
+  // Explicit
+  nameParrentString = 'App Component';
+  nameChildString = 'Child Component';
+
+  numberParrentInt = 0;
+
+  onClickIncreaseNumberParrent() {
+
+    this.numberParrentInt = this.numberParrentInt + 1;
+
+  }
+
+
+
+} // AppComponent Class
---
+++ src/app/components/childcomponent/childcomponent.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-childcomponent',
+  templateUrl: './childcomponent.component.html',
+  styleUrls: ['./childcomponent.component.css']
+})
+export class ChildcomponentComponent implements OnInit {
+
+  // Explicit
+  @Input() nameFromParrent: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
